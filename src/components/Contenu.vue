c7ad49c7e9edd2d4320d49d1dcf467248fa08262
Omni <geoffrey.frioli@gmail.com>
2020-08-13 14:50:53 +0200

    import component

---
+++ src/components/Contenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ txt }}</div>
+</template>
+
+
+
+<script>
+
+  export default {
+    data() {
+      return {
+        txt: 'Je suis le contenu'
+      }
+    }
+  }
+</script>
+
+
+
+<style></style>
